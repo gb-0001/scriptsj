fe56a580669380669bce6e0cae67e8baa5407269
gb-0001 <g.b0001@outlook.fr>
2022-02-04 12:42:36 +0100

    add getcred file maj

--- python/modules/md_sec_getcred.py
+++ python/modules/md_sec_getcred.py
@@ -7,6 +7,7 @@ def mdgetcred(pathfile):
         #take line 2
         for i in range(2):
             line = myFile.readline()
-        login1,passwd1 = line2.split(";")
+        login1,passwd1 = line.split(";")
     login1 = login1.rstrip('\n')
     passwd1 = passwd1.rstrip('\n')
+    return login1,passwd1
