9440d3a09a506edcacfac8e290610bb3ea295601
gb-0001 <g.b0001@outlook.fr>
2022-02-15 09:16:37 +0100

    cnx ssh key sur echange de cle

--- python/modules/md_cnx_ssh.py
+++ python/modules/md_cnx_ssh.py
@@ -8,7 +8,13 @@ import subprocess
 import paramiko
 
 
+
+
 # Prerequisite to have done the key exchange beforehand for user
+# example if the key does not exist in the target ~/.ssh/id_rsa 
+# ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+# if the key does exist in the target ~/.ssh/id_rsa    ==>DO
+# /usr/bin/sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub root@IPCENTRALSOURCE
 #ex. mdsshcmd(servername,"user","/home/user/.ssh/id_rsa.pub","hostname")
 def mdsshcmd(hostname,user,pubkeypath,cmdtopass):
     ssh = paramiko.SSHClient()
