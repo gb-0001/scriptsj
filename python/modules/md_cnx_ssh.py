aba973f0470b1686d36ad153ad2c2732079aa806
gb-0001 <g.b0001@outlook.fr>
2022-02-18 09:14:19 +0100

    maj ssh cnx

--- python/modules/md_cnx_ssh.py
+++ python/modules/md_cnx_ssh.py
@@ -7,14 +7,11 @@ import subprocess
 
 import paramiko
 
-
-
-
 # Prerequisite to have done the key exchange beforehand for user
 # example if the key does not exist in the target ~/.ssh/id_rsa 
 # ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
 # if the key does exist in the target ~/.ssh/id_rsa    ==>DO
-# /usr/bin/sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub root@IPCENTRALSOURCE
+# /usr/bin/sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa.pub root@IPSOURCE
 #ex. mdsshcmd(servername,"user","/home/user/.ssh/id_rsa.pub","hostname")
 def mdsshcmd(hostname,user,pubkeypath,cmdtopass):
     ssh = paramiko.SSHClient()
@@ -25,7 +22,7 @@ def mdsshcmd(hostname,user,pubkeypath,cmdtopass):
 #    print("IN",ssh_stdin.read())
 #    print("ERR",ssh_stderr.read())
     returnvalue=ssh_stdout.read()
-    print(ssh_stdin, ssh_stdout, ssh_stderr)
+    print("Retour md STDIN:",ssh_stdin,"\nRetour md STDOUT:", ssh_stdout,"\nRetour md STDERR:", ssh_stderr)
     ssh.close()
     return returnvalue
 
@@ -38,6 +35,6 @@ def mdssholdcmd(hostname,user,passwd1,cmdtopass):
 #    print("IN",ssh_stdin.read())
 #    print("ERR",ssh_stderr.read())
     returnvalue=ssh_stdout.read()
-    print(ssh_stdin, ssh_stdout, ssh_stderr)
+    print("Retour md STDIN:",ssh_stdin,"\nRetour md STDOUT:", ssh_stdout,"\nRetour md STDERR:", ssh_stderr)
     ssh.close()
     return returnvalue
